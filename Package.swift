3b70915ea54cbf737df183e5fa22ff5c035882f8
osama  alhour <osama.id78@gmail.com>
2022-10-16 23:49:09 +0300

    initial commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SantanderWrappers",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(name: "ApplicationsWrapper", targets: ["ApplicationsWrapper"]),
+        .library(name: "AssetCatalogWrapper", targets: ["AssetCatalogWrapper"])
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(name: "ApplicationsWrapper", dependencies: ["CFrameworks"]),
+        .target(name: "AssetCatalogWrapper", dependencies: ["CFrameworks"]),
+        .systemLibrary(name: "CFrameworks", path: nil, pkgConfig: nil, providers: nil)
+    ]
+)
