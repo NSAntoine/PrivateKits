e1a34ad9d3d9922be7c694755d30d3eef73f9f8d
osama  alhour <osama.id78@gmail.com>
2022-10-17 22:23:42 +0300

    bring back fsoperations

--- Package.swift
+++ Package.swift
@@ -10,6 +10,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "ApplicationsWrapper", targets: ["ApplicationsWrapper"]),
         .library(name: "AssetCatalogWrapper", targets: ["AssetCatalogWrapper"]),
+        .library(name: "FSOperations", targets: ["FSOperations"]),
         .library(name: "NSTask", targets: ["NSTask"])
     ],
     dependencies: [
@@ -21,6 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(name: "ApplicationsWrapper", dependencies: ["CFrameworks"]),
         .target(name: "AssetCatalogWrapper", dependencies: ["CFrameworks"]),
+        .target(name: "FSOperations", dependencies: ["AssetCatalogWrapper"]),
         .target(name: "NSTask", dependencies: ["CFrameworks"]),
         .systemLibrary(name: "CFrameworks", path: nil, pkgConfig: nil, providers: nil)
     ]
---
+++ Sources/FSOperations/RootHelperConfiguration.swift
@@ -0,0 +1,16 @@
+//
+//  RootHelperConfiguration.swift
+//  
+//
+//  Created by Serena on 17/10/2022
+//
+
+
+import Foundation
+
+public protocol RootHelperConfiguration {
+    typealias ActionHandler = ((FSOperation, URL) -> Void)
+    
+    var action: ActionHandler { get set }
+    var useRootHelper: Bool { get }
+}
